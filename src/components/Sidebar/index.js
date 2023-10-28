4c67f62edaf427a4d38f5f20d7eb43344691571e
Souleymane DIALLO <diallo.souleymane1@uam.edu.sn>
2023-10-28 11:51:06 +0000

    implémentation de la barre de navigation

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,49 +3,78 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/my-name.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo' />
-            <img className='sub-logo' src={LogoSubtitle} alt='Souleymane' />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-            </NavLink>
-        </nav>
+const Sidebar = () => {
+    // Navigation bar sur les petites tailles d'écran
+    const [showNav, setShowNav] = useState(false);
+    // false car nous ne voulons que la barre de navigation soit affichée au chargement de la page
+    
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoS} alt='logo' />
+                <img className='sub-logo' src={LogoSubtitle} alt='Souleymane' />
+            </Link>
+            {/* 
+                A présent, nous utilisons une classe pour l'affichage de la barre
+                de navigation sur les petits écrans
+            */}
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faSuitcase} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                </NavLink>
+                {/* 
+                    Mise en place d'une icone de fermeture du menu, une fois ouvert
+                */}
+                <FontAwesomeIcon
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color='#ffd700'
+                    size='3x'
+                    className='close-mobile-navBar'
+                />
+            </nav>
 
-        <ul>
-            <li>
-                <a 
-                target='_blank'
-                rel='noreferrer'
-                href='https://www.linkedin.com/in/souleymane-diallo-17591422a/'>
-                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-                </a>
-            </li>
-            <li>
-                <a 
-                target='_blank'
-                rel='noreferrer'
-                href='https://github.com/DIALLOSouleymane'>
-                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
-                </a>
-            </li>
-        </ul>
-    </div>
-);
+            <ul>
+                <li>
+                    <a 
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://www.linkedin.com/in/souleymane-diallo-17591422a/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://github.com/DIALLOSouleymane'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+            {/* Gestion de la responsivité : mise en place d'un menu hamburger */}
+            <FontAwesomeIcon 
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color='#ffd700'
+                className='hamburger-icon'
+                size='3x'
+            />
+        </div>
+    );
+}
 
 export default Sidebar;
